e1c1f209e777d4fb270fe143f23b713953a28301
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2022-11-03 10:38:10 +0300

    Improving logging

--- Utils/pretrainedGloVe.py
+++ Utils/pretrainedGloVe.py
@@ -1,9 +1,11 @@
 import numpy as np
+from Utils.logger import logger
 
 filename= '../data/glove.6B.100d.txt'
 
 class pretrainedWordEmeddings:
     def __init__(self, filename):
+        logger.info(f'Loading pretrained word emeddings from: {filename}')
         with open(filename) as f:
             self.word2embeddings = f.readlines()
         self.word2embeddings = self.get_pretrained_word2embeddings(self.word2embeddings)
@@ -15,13 +17,18 @@ class pretrainedWordEmeddings:
             word = values[0]
             coefs = np.asarray(values[1:], dtype='float32')
             word2embeddings[word] = coefs
-        print('pretrained word vectors loaded - {}'.format(len(word2embeddings)))
+        
+        logger.info(f'Loaded {len(word2embeddings)} pretrained word vectors')
+        
         return word2embeddings
+    
     def get_embedding(self, word):
         try:
             embedding = self.word2embeddings[word]
         except:
+            logger.warning(f'Could not find word embedding for: {word}, using zeros!');
             embedding = np.zeros((100)).astype('float32')
+        
         return embedding
 
 if __name__ == "__main__":
